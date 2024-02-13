7c07c52852d1ba3a5d235e16d4b707094f0489e6
Aiden Blinn <aidenblinn@Aidens-MacBook-Pro.local>
2024-02-13 16:45:48 -0500

    update state and state setters to simulate desktop application open/close functionality

--- src/app/computerPrograms/programColumns.tsx
+++ src/app/computerPrograms/programColumns.tsx
@@ -1,87 +1,83 @@
 import Email from "./email.tsx";
 import { Program } from "../../../types.ts";
 
+const programs: Program[] = [
+  {
+    programModal: Email,
+    name: "Email",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+  {
+    programModal: Email,
+    name: "Clock",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+  {
+    programModal: Email,
+    name: "Folder",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+  {
+    programModal: Email,
+    name: "LinkedIn",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+  {
+    programModal: Email,
+    name: "Music",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+  {
+    programModal: Email,
+    name: "Paint",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+  {
+    programModal: Email,
+    name: "Trash",
+    size: {
+      initHeight: 300,
+      initWidth: 400,
+      minHeight: 100,
+      minWidth: 100,
+    },
+  },
+];
+
 /**
  * Each sub-array represents a column of programs to be displayed on the desktop
  */
 export const programColumns: Program[][] = [
-  [
-    {
-      programModal: Email,
-      name: "Email",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-    {
-      programModal: Email,
-      name: "Clock",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-    {
-      programModal: Email,
-      name: "Folder",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-    {
-      programModal: Email,
-      name: "LinkedIn",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-  ],
-  [
-    {
-      programModal: Email,
-      name: "Music",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-    {
-      programModal: Email,
-      name: "Paint",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-    {
-      programModal: Email,
-      name: "Trash",
-      size: {
-        initHeight: 300,
-        initWidth: 400,
-        minHeight: 100,
-        minWidth: 100,
-      },
-    },
-  ],
+  programs.slice(0, 4),
+  programs.slice(4, 7),
 ];
-
-export const programCount: number = programColumns.reduce(
-  (acc, curr) => acc + curr.length,
-  0
-);
