6aa4faade96d63f0bbb500d63a7bad5329949647
Aiden Blinn <aidenblinn@Aidens-MacBook-Pro.local>
2024-02-13 16:42:58 -0500

    add desktop state type

--- types.ts
+++ types.ts
@@ -1,5 +1,7 @@
-// Stores information about an individual program,
-// which is rendered as a pop-up modal on the simulated desktop
+/**
+ * Stores information about an individual program,
+ * which is rendered as a pop-up modal on the simulated desktop
+ */
 export type Program = {
   programModal: () => React.ReactElement;
   name: string;
@@ -9,4 +11,23 @@ export type Program = {
     minHeight: number;
     minWidth: number;
   };
+};
+
+/**
+ * Stores information about the state of programs on the simulated desktop
+ */
+export type DesktopState = {
+  /** Programs are layered by z-index to simulate a multi-window desktop.
+   * Program modals are prioritized by most recent open / focus.
+   */
+  modalHierarchy: string[];
+  /**
+   * List of open programs to be rendered in task bar,
+   * ordered left to right by first program opened.
+   */
+  taskBarPrograms: string[];
+  /** Active program (most recently opened / focused program)
+   * will be indicated in task bar and in modal border color.
+   */
+  activeProgram: string | null;
 };
