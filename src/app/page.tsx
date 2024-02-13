ba42d6ca63350a1cf0dd5fd30d3c4e96834f4677
Aiden Blinn <aidenblinn@Aidens-MacBook-Pro.local>
2024-02-13 16:45:48 -0500

    update state and state setters to simulate desktop application open/close functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,46 +3,50 @@ import ProgramWrapper from "./computerPrograms/programWrapper.tsx";
 import Clock from "./desktopUtilities/clock.tsx";
 import TaskBar from "./desktopUtilities/taskBar.tsx";
 import { useState } from "react";
-import {
-  programColumns,
-  programCount,
-} from "./computerPrograms/programColumns.tsx";
-import { Program } from "../../types.ts";
+import { programColumns } from "./computerPrograms/programColumns.tsx";
+import { DesktopState, Program } from "../../types.ts";
 
 export default function Home() {
-  /** Programs are layered by z-index to simulate a multi-window desktop.
-   * Program modals are prioritized by most recent open / focus.
-   */
-  const [modalHierarchy, setModalHierarchy] = useState<string[]>(
-    new Array(programCount)
-  );
-
-  /**
-   * List of open programs to be rendered in task bar,
-   * ordered left to right by first program opened.
-   */
-  const [taskBarPrograms, setTaskBarPrograms] = useState<string[]>([]);
-
-  /** Active program (most recently opened / focused program)
-   * will be indicated in task bar.
-   */
-  const [activeTaskBarProgram, setActiveTaskBarProgram] = useState<
-    string | null
-  >(null);
+  const [desktopDisplay, setDesktopDisplay] = useState<DesktopState>({
+    modalHierarchy: [],
+    taskBarPrograms: [],
+    activeProgram: null,
+  });
 
   /**
    * Focus program by layering it on top of all other programs
    * and by setting its task bar icon as active.
    * This function is triggered when a user clicks on a program's modal
    * or clicks on a program's task bar icon.
-   * @param programName
+   * @param programName Name of program to focus
+   * @param openingProgram Boolean indicating whether program is being opened
    */
-  const focusProgram = (programName: string): void => {
-    // Move program to end of hierarchy array (highest z-index)
-    setModalHierarchy(
-      modalHierarchy.filter((el) => el !== programName).concat([programName])
-    );
-    setActiveTaskBarProgram(programName);
+  const focusProgram = (
+    programName: string,
+    openingProgram?: boolean
+  ): void => {
+    const taskBarPrograms = desktopDisplay.taskBarPrograms;
+    // Add new program to task bar if opening program
+    // and if program not already in task bar
+    if (
+      openingProgram &&
+      !desktopDisplay.taskBarPrograms.includes(programName)
+    ) {
+      taskBarPrograms.push(programName);
+    }
+
+    setDesktopDisplay({
+      taskBarPrograms,
+      activeProgram: programName,
+      // Move program to end of hierarchy array (highest z-index)
+      modalHierarchy: desktopDisplay.modalHierarchy
+        .filter((el) => el !== programName)
+        .concat([programName]),
+    });
+  };
+
+  const setDesktopDisplayHelper = (input: DesktopState) => {
+    setDesktopDisplay(input);
   };
 
   return (
@@ -57,11 +61,10 @@ export default function Home() {
               <ProgramWrapper
                 program={program}
                 focusProgram={focusProgram}
-                zIndex={modalHierarchy.indexOf(program.name)}
-                taskBarPrograms={taskBarPrograms}
-                setTaskBarPrograms={setTaskBarPrograms}
-                modalHierarchy={modalHierarchy}
-                setModalHierarchy={setModalHierarchy}
+                zIndex={desktopDisplay.modalHierarchy.indexOf(program.name)}
+                desktopDisplay={desktopDisplay}
+                setDesktopDisplay={setDesktopDisplay}
+                isActive={desktopDisplay.activeProgram === program.name}
               />
             ))}
           </div>
@@ -73,13 +76,11 @@ export default function Home() {
           <p className="text-white">Start</p>
         </div>
         {/* Task Bar */}
-        <div className="flex-1">
-          <TaskBar
-            taskBarPrograms={taskBarPrograms}
-            activeTaskBarProgram={activeTaskBarProgram}
-            focusProgram={focusProgram}
-          />
-        </div>
+        <TaskBar
+          taskBarPrograms={desktopDisplay.taskBarPrograms}
+          activeProgram={desktopDisplay.activeProgram}
+          focusProgram={focusProgram}
+        />
         {/* Clock */}
         <div className="flex items-center w-fit h-full px-2 bg-blue-400">
           <Clock />
