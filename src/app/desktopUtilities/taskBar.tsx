c85cd0c2b34f0009ee04533cfc4e62ebb75b5da3
Aiden Blinn <apblinn@gmail.com>
2024-02-15 15:21:26 -0500

    taskbar styling

--- src/app/desktopUtilities/taskBar.tsx
+++ src/app/desktopUtilities/taskBar.tsx
@@ -1,33 +1,31 @@
 type taskBarProps = {
   taskBarPrograms: string[];
   activeProgram: string | null;
-  focusProgram: (programName: string) => void;
+  focusProgram: (name: string) => void;
 };
+
+const baseStyle: string =
+  "flex flex-wrap justify-start content-center h-full w-[120px] px-2 text-white rounded hover:cursor-pointer";
+const inactiveStyle: string =
+  baseStyle + " bg-blue-400 shadow-[inset_0_2px_4px_0_rgb(255,255,255,0.3)]";
+const activeStyle: string =
+  baseStyle + " bg-blue-700 shadow-[inset_0_2px_4px_0_rgb(0,0,0,0.3)]";
 
 export default function TaskBar({
   taskBarPrograms,
   activeProgram,
   focusProgram,
 }: taskBarProps): React.ReactElement {
-  const inactiveStyle: string =
-    "flex flex-wrap justify-start content-center h-full w-[120px] px-2 bg-blue-400 text-white rounded hover:cursor-pointer";
-  const activeStyle: string = inactiveStyle.replace(
-    "text-white",
-    "text-red-500"
-  );
-
   return (
     <div className="flex-1 flex justify-start h-full p-1 gap-x-1">
-      {taskBarPrograms.map((programName: string) => (
+      {taskBarPrograms.map((name: string) => (
         <div
-          className={
-            programName === activeProgram ? activeStyle : inactiveStyle
-          }
-          onClick={() => focusProgram(programName)}
-          key={`${programName}-taskbar`}
+          className={name === activeProgram ? activeStyle : inactiveStyle}
+          onClick={() => focusProgram(name)}
+          key={`${name}-taskbar`}
         >
-          <img className="h-6 mr-2" src={`icons/${programName}.ico`} />
-          <p className="h-fit">{programName}</p>
+          <img className="h-6 mr-2" src={`icons/${name}.ico`} />
+          <p className="h-fit">{name}</p>
         </div>
       ))}
     </div>
