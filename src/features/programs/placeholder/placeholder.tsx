8ee36103a90433992698201c8008b89c701e5b14
Aiden Blinn <apblinn@gmail.com>
2024-02-16 18:12:33 -0500

    create placeholder component while programs under development

---
+++ src/features/programs/placeholder/placeholder.tsx
@@ -0,0 +1,7 @@
+export default function Placeholder() {
+  return (
+    <div className="flex justify-center items-center h-full bg-yellow-200">
+      <p className="text-black text-center text-3xl">Under Construction</p>
+    </div>
+  );
+}
