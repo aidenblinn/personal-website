5fe876c8433355434deb962c11e18a5de416c17a
Aiden Blinn <apblinn@gmail.com>
2024-02-16 17:46:54 -0500

    merge utility bar tools and state into single function

---
+++ src/features/utilityBar/tools/toolsSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const getTime = () => {
+  const dateObject = new Date();
+
+  const hour = dateObject.getHours();
+  const minute = dateObject.getMinutes();
+
+  const timeString =
+    /* Hour */ (hour % 12 === 0 ? "12" : (hour % 12).toString()) +
+    /* Colon */ ":" +
+    /* Minute */ (minute < 10 ? "0" + minute.toString() : minute.toString()) +
+    /* AM/PM */ (hour > 11 ? "PM" : "AM");
+
+  return timeString;
+};
+
+const initialState: {
+  muted: boolean;
+  time: string;
+} = {
+  muted: false,
+  time: getTime(),
+};
+
+export const toolsSlice = createSlice({
+  name: "tools",
+  initialState,
+  reducers: {
+    toggleMute: (state) => {
+      state.muted = !state.muted;
+    },
+    updateTime: (state) => {
+      state.time = getTime();
+    },
+  },
+});
+
+export const { toggleMute, updateTime } = toolsSlice.actions;
+
+export default toolsSlice.reducer;
