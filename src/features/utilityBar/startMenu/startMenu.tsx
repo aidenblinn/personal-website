c436124121be35890025360c3c9dcb9ebf48c6a1
Aiden Blinn <apblinn@gmail.com>
2024-02-16 23:18:10 -0500

    create separate start menu component

---
+++ src/features/utilityBar/startMenu/startMenu.tsx
@@ -0,0 +1,7 @@
+export default function StartMenu(): React.ReactElement {
+  return (
+    <div className="flex items-center w-fit h-full px-2 bg-green-500">
+      <h1 className="text-white">Start</h1>
+    </div>
+  );
+}
