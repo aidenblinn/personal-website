71cc10fdff75d489ea6ed1804831c78c66531ceb
Aiden Blinn <apblinn@gmail.com>
2024-02-16 20:44:53 -0500

    define separate icon types based on functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { Provider } from "react-redux";
 import Program from "../features/programs/program.tsx";
+import Icon from "../features/icon.tsx";
 import UtilityBar from "../features/utilityBar/utilityBar.tsx";
-import { programColumns } from "../config/programs.ts";
-import { ProgramType } from "../../types.ts";
+import { iconColumns } from "../config/programs.ts";
+import { IconType } from "../../types.ts";
 import store from "./store.ts";
 
 export default function Desktop() {
@@ -11,14 +12,23 @@ export default function Desktop() {
     <Provider store={store}>
       <main className="flex flex-col h-screen">
         <div className="flex grow w-screen bg-bliss bg-center content-start p-2">
-          {programColumns.map((programArray: ProgramType[], index: number) => (
+          {iconColumns.map((iconArray: IconType[], index: number) => (
             <div
               className="w-fit h-fit grid grid-cols-1 gap-4"
               key={`column#${index}`}
             >
-              {programArray.map((program: ProgramType) => (
-                <Program key={`${program.name}-program`} program={program} />
-              ))}
+              {iconArray.map((icon: IconType) => {
+                if (icon.type === "program") {
+                  return (
+                    <>
+                      <Icon icon={icon} />
+                      <Program key={`${icon.name}-program`} program={icon} />
+                    </>
+                  );
+                } else {
+                  return <Icon icon={icon} />;
+                }
+              })}
             </div>
           ))}
         </div>
