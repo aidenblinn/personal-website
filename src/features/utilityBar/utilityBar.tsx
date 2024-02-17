6ebc4a2769be59e9750e456ccd328d8b9b9de741
Aiden Blinn <apblinn@gmail.com>
2024-02-16 23:18:10 -0500

    create separate start menu component

--- src/features/utilityBar/utilityBar.tsx
+++ src/features/utilityBar/utilityBar.tsx
@@ -1,16 +1,12 @@
 import TaskBar from "./taskBar/taskBar.tsx";
 import Tools from "./tools/tools.tsx";
+import StartMenu from "./startMenu/startMenu.tsx";
 
 export default function UtilityBar(): React.ReactElement {
   return (
     <div className="flex justify-between items-center w-screen h-12 bg-bliss-blue">
-      {/* Start Menu */}
-      <div className="flex items-center w-fit h-full px-2 bg-green-500">
-        <h1 className="text-white">Start</h1>
-      </div>
-      {/* Task Bar containing open programs */}
+      <StartMenu />
       <TaskBar />
-      {/* Tools to the right of the task bar */}
       <Tools />
     </div>
   );
