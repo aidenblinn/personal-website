1a33a2c4b74462f98ef3916b831da22899d6952a
Aiden Blinn <apblinn@gmail.com>
2024-02-16 23:17:32 -0500

    rename files to match usage

--- src/features/desktop.tsx
+++ src/features/desktop.tsx
@@ -1,32 +1,36 @@
 "use client";
+import React from "react";
 import { Provider } from "react-redux";
-import Program from "../features/programs/program.tsx";
-import Icon from "../features/icon.tsx";
-import UtilityBar from "../features/utilityBar/utilityBar.tsx";
-import { iconColumns } from "../config/programs.ts";
+import Program from "./programs/program.tsx";
+import Icon from "./icon.tsx";
+import UtilityBar from "./utilityBar/utilityBar.tsx";
+import { iconColumns } from "../config/icons.ts";
 import { IconType } from "../../types.ts";
-import store from "./store.ts";
+import store from "../app/store.ts";
 
 export default function Desktop() {
   return (
     <Provider store={store}>
       <main className="flex flex-col h-screen">
-        <div className="flex grow w-screen bg-bliss bg-center content-start p-2">
+        <div
+          id="desktop"
+          className="flex grow w-screen bg-bliss bg-center content-start p-2"
+        >
           {iconColumns.map((iconArray: IconType[], index: number) => (
             <div
-              className="w-fit h-fit grid grid-cols-1 gap-4"
               key={`column#${index}`}
+              className="w-fit h-fit grid grid-cols-1 gap-4"
             >
               {iconArray.map((icon: IconType) => {
                 if (icon.type === "program") {
                   return (
-                    <>
-                      <Icon icon={icon} />
+                    <React.Fragment key={`${icon.name}-fragment`}>
+                      <Icon key={`${icon.name}-icon`} icon={icon}></Icon>
                       <Program key={`${icon.name}-program`} program={icon} />
-                    </>
+                    </React.Fragment>
                   );
                 } else {
-                  return <Icon icon={icon} />;
+                  return <Icon key={`${icon.name}-icon`} icon={icon} />;
                 }
               })}
             </div>
