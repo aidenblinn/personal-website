eee1dc89be1c10bee304b7a70c56f8a6e55edcf8
Aiden Blinn <apblinn@gmail.com>
2024-02-16 20:45:21 -0500

    minor design edits

--- src/features/programs/placeholder/placeholder.tsx
+++ src/features/programs/placeholder/placeholder.tsx
@@ -1,7 +1,12 @@
 export default function Placeholder() {
   return (
-    <div className="flex justify-center items-center h-full bg-yellow-200">
-      <p className="text-black text-center text-3xl">Under Construction</p>
+    <div className="flex flex-col justify-center items-center h-full bg-yellow-200">
+      <img
+        src={"icons/UnderConstruction.ico"}
+        alt="Construction Worker"
+        className="h-20 mb-4"
+      />
+      <p className="text-center text-3xl">Under Construction</p>
     </div>
   );
 }
