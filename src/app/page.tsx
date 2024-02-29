acc80e7878f0ffa39f026ebf08427fcd8df816d5
Aiden Blinn <apblinn@gmail.com>
2024-02-28 21:15:36 -0500

    move desktop functionality to separate component inside of new computer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,12 @@
 "use client";
-import React from "react";
 import { Provider } from "react-redux";
-import Program from "../features/programs/program.tsx";
-import Icon from "../features/icon.tsx";
-import UtilityBar from "../features/utilityBar/utilityBar.tsx";
-import { iconColumns } from "../config/icons.ts";
-import { IconType } from "../../types.ts";
 import store from "./store.ts";
+import Computer from "../features/computer.tsx";
 
 export default function Desktop() {
   return (
     <Provider store={store}>
-      <main className="flex flex-col h-screen">
-        <div
-          id="desktop"
-          className="flex grow w-screen bg-bliss bg-cover bg-center content-start p-2"
-        >
-          {iconColumns.map((iconArray: IconType[], index: number) => (
-            <div
-              key={`column#${index}`}
-              className="w-fit h-fit grid grid-cols-1 gap-4"
-            >
-              {iconArray.map((icon: IconType) => {
-                if (icon.type === "program") {
-                  return (
-                    <React.Fragment key={`${icon.name}-fragment`}>
-                      <Icon
-                        key={`${icon.name}-icon`}
-                        icon={icon}
-                        isLink={false}
-                      ></Icon>
-                      <Program key={`${icon.name}-program`} program={icon} />
-                    </React.Fragment>
-                  );
-                } else {
-                  return (
-                    <React.Fragment key={`${icon.name}-fragment`}>
-                      <img
-                        className="absolute top-0 right-0 h-2"
-                        src="icons/Link.ico"
-                        alt=""
-                      />
-                      <Icon
-                        key={`${icon.name}-icon`}
-                        icon={icon}
-                        isLink={true}
-                      ></Icon>
-                    </React.Fragment>
-                  );
-                }
-              })}
-            </div>
-          ))}
-        </div>
-        <UtilityBar />
-      </main>
+      <Computer />
     </Provider>
   );
 }
---
+++ src/features/desktop/desktop.tsx
@@ -0,0 +1,55 @@
+import Program from "./programs/program.tsx";
+import Icon from "./programs/icon.tsx";
+import UtilityBar from "./utilityBar/utilityBar.tsx";
+import { iconColumns } from "../../config/icons.ts";
+import { IconType } from "../../../types.ts";
+import React from "react";
+
+export default function Desktop(): React.ReactElement {
+  return (
+    <main className="flex flex-col h-screen">
+      <div
+        id="desktop"
+        className="flex grow w-screen bg-bliss bg-cover bg-center content-start p-2"
+      >
+        {iconColumns.map((iconArray: IconType[], index: number) => (
+          <div
+            key={`column#${index}`}
+            className="w-fit h-fit grid grid-cols-1 gap-4"
+          >
+            {iconArray.map((icon: IconType) => {
+              if (icon.type === "program") {
+                return (
+                  <React.Fragment key={`${icon.name}-fragment`}>
+                    <Icon
+                      key={`${icon.name}-icon`}
+                      icon={icon}
+                      isLink={false}
+                    ></Icon>
+                    <Program key={`${icon.name}-program`} program={icon} />
+                  </React.Fragment>
+                );
+              } else {
+                return (
+                  <React.Fragment key={`${icon.name}-fragment`}>
+                    <img
+                      className="absolute top-0 right-0 h-2"
+                      src="icons/Link.ico"
+                      alt=""
+                    />
+                    <Icon
+                      key={`${icon.name}-icon`}
+                      icon={icon}
+                      isLink={true}
+                    ></Icon>
+                  </React.Fragment>
+                );
+              }
+            })}
+          </div>
+        ))}
+      </div>
+      <UtilityBar />
+    </main>
+  );
+}
