badd59fd7f7024cfe47ce399bc94af799c6433ee
Aiden Blinn <apblinn@gmail.com>
2024-03-28 17:01:07 -0400

    login page spacing and login state handling

--- src/features/computer.tsx
+++ src/features/computer.tsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
 import { useAppSelector } from "../app/hooks.ts";
 import Login from "./login/login.tsx";
 import Desktop from "./desktop/desktop.tsx";
 
 export default function Computer(): React.ReactElement {
   const loggedIn = useAppSelector((state) => state.login.loggedIn);
+  const muted = useAppSelector((state) => state.tools.muted);
+
+  useEffect(() => {
+    if (loggedIn && !muted) {
+      const loginSound = new Audio("sounds/startup.mp3");
+      loginSound.play();
+    }
+  }, [loggedIn]);
+
   return (
     <>
       {/* Login page disabled while under development */}
-      {/* {!loggedIn && <Login />} */}
+      {!loggedIn && <Login />}
       <Desktop />
     </>
   );
