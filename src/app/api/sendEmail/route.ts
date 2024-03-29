2893fb7917ca4808de3c8b7f2ebde64bfc5bbea1
Aiden Blinn <apblinn@gmail.com>
2024-03-29 14:31:07 -0400

    catch email submission errors from resend API call

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -5,19 +5,25 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
-    const formData = await req.json();
+    const emailData = await req.json();
     if (process.env.RECIPIENT_EMAIL === undefined) {
       throw new Error("Recipient email environment variable is not set");
     }
-    const emailRequest = await resend.emails.send({
-      from: `Contact Form <contact@aidenblinn.com>`,
+    const emailResponse = await resend.emails.send({
+      from: "Personal Site Guest <contact@aidenblinn.com>",
       to: [process.env.RECIPIENT_EMAIL],
-      subject: formData.subject,
-      react: EmailTemplate({ content: formData.content }),
+      subject: emailData.subject,
+      react: EmailTemplate({
+        from: emailData.from,
+        content: emailData.content,
+      }),
     });
 
-    return Response.json(emailRequest);
+    if (emailResponse.error) {
+      throw new Error(emailResponse.error.message);
+    }
+    return new Response(JSON.stringify(emailResponse), { status: 200 });
   } catch (error) {
-    return Response.json({ error });
+    return new Response(JSON.stringify({ error }), { status: 500 });
   }
 }
