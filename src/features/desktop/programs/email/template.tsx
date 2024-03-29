76e3e27a5fc45c292b75b88f5583f725c9f18d13
Aiden Blinn <apblinn@gmail.com>
2024-03-29 14:31:19 -0400

    add sender info to email template

--- src/features/desktop/programs/email/template.tsx
+++ src/features/desktop/programs/email/template.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 
 export default function EmailTemplate({
+  from,
   content,
 }: {
+  from: string;
   content: string;
 }): React.ReactElement {
   return (
     <div>
+      <h1>
+        <strong>From: </strong>
+        {from.trim()}
+      </h1>
       <p>{content}</p>
     </div>
   );
