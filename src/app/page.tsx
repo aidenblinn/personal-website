2a6c870037e16efa9390c7474f0187fb9ac7c82a
Aiden Blinn <apblinn@gmail.com>
2024-05-17 12:06:48 -0400

    remove testing flag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@
 import { Provider } from "react-redux";
 import store from "./store.ts";
 import Computer from "../features/computer.tsx";
-import SnakeProgram from "@/features/desktop/programs/snake/index.tsx";
 
 export default function Desktop() {
   return (
     <Provider store={store}>
-      <SnakeProgram />
+      <Computer />
     </Provider>
   );
 }
